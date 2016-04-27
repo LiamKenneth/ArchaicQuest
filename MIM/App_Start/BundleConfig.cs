e2220171031de5ae2c59e86e0a6baedfbfbc4cfb
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-04-27 14:25:24 +0100

    Front End Tidy up
    Front End Tidy up, Backend next

--- MIM/App_Start/BundleConfig.cs
+++ MIM/App_Start/BundleConfig.cs
@@ -14,18 +14,16 @@ namespace MIM
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/UI").Include(
+                  "~/Content/bootstrap.min.css",
+                  "~/UI/Css/master.min.css"
+                ));
         }
     }
 }
--- MIM/UI/JS/MIM.js
+++ MIM/UI/JS/MIM.js
@@ -1,13 +1,71 @@
 ﻿$(function () {
-    // Reference the auto-generated proxy for the hub.
+
+    //================================================================================
+    // Setup the auto-generated proxy for the hub.
+    //================================================================================
+
     var chat = $.connection.mIMHub;
-    // Create a function that the hub can call back to display messages.
-    chat.client.addNewMessageToPage = function (message) {
-        // Add the message to the page.
-        $('#discussion').append("<p>" + htmlEncode(message) + "</p>");
+    var client = chat.client;
+    var server = chat.server;
+
+    //================================================================================
+    // Helper Functions
+    //================================================================================
+    var MIM = {
+        getRaceInfo: function (getValue) {
+            server.pickRace(getValue);
+        },
+        selectRace: function () {
+            $(".modal-body a").click(function () {
+
+                if ($(this).hasClass("active")) {
+
+                    //don't call server if same tab
+                    return false;
+                }
+                else {
+                    $(".modal-body a").removeClass("active");
+                    $(this).addClass("active");
+
+                    var getValue = $(this).text().trim().toLowerCase().toString();
+
+                    MIM.getRaceInfo(getValue)
+                }
+            });
+        },
+        sendMessageToServer: function() {
+            $('#sendmessage').click(function () {
+
+                var message = $('#message');
+
+                // Call the Send method on the hub.
+                server.recieveFromClient(message.val());
+
+                message.select().focus();
+            });
+        },
+        htmlEncode: function (value) {
+            var encodedValue = $('<div />').text(value).html();
+            return encodedValue;
+        }
+    }
+
+
+    //// Set focus to input box ////
+    $('#message').focus();
+
+    //================================================================================
+    // Client Functions
+    //================================================================================
+
+
+    //// Add a new message to the page ////
+    client.addNewMessageToPage = function (message) {
+        $('#discussion').append("<p>" + MIM.htmlEncode(message) + "</p>");
     };
 
-    chat.client.updateRaceInfo = function (dataName, dataHelp, dataImg) {
+    //// Update Race Info ////
+    client.updateRaceInfo = function (dataName, dataHelp, dataImg) {
 
         var raceInfo = "<h2>" + dataName + "</h2>" + "<p>" + dataHelp + "</p>";
 
@@ -16,96 +74,32 @@
         $('#raceImg').attr('src', dataImg);
     }
 
-
-    var name = "";
-    var sex = "male"
-    var selectedClass = "Mage"
-    var strength = 0;
-    var dexterity = 0;
-    var constitution = 0;
-    var wisdom = 0;
-    var intelligence = 0;
-    var charisma = 0;
-
-    chat.client.createCharacter = function () {
-      //  name = prompt("Please enter your name", "Malleus");
-
+    //// generate Stats ////
+    client.setStats = function (stats) {
+        return stats;
     }
 
-    chat.client.setStats = function (stats) {
-        console.log("stats " + stats[0]);
 
-        strength = stats[0];
-        dexterity = stats[1];
-        constitution = stats[2];
-        wisdom = stats[3];
-        intelligence = stats[4];
-        charisma = stats[5];
-       // prompt("str: " + strength + " dex:" + dexterity + " con:" + constitution + " wis:" + wisdom + " int:" + intelligence + " cha:" + charisma + " Are you hapy with these stats?");
 
-        chat.server.loadRoom();
- 
-    }
-
-    // Set initial focus to message input box.
-    $('#message').focus();
-    // Start the connection.
+    //================================================================================
+    // Hub has loaded & Server functions
+    //================================================================================
     $.connection.hub.start().done(function () {
 
-        getRaceInfo("hum"); //set default;
-        chat.server.welcome();
+        //// Load 1st race choice
+         MIM.getRaceInfo("human"); //set default;
 
-        chat.client.createCharacter();
+        //server.welcome();
+        server.loadRoom();
    
-        var stats = chat.server.getStats();
+        /// send info to server
+        MIM.sendMessageToServer();
 
-        if (stats) {
-            console.log("stats " + stats[0])
-        }
-
-
-        $('#sendmessage').click(function () {
-
-            
-            // Call the Send method on the hub.
-            chat.server.recieveFromClient($('#message').val());
-            // Clear text box and reset focus for next comment.
-            $('#message').val('').focus();
-        });
-
-        function getRaceInfo(getValue) {
-    
-          chat.server.pickRace(getValue);
-           
-        }
-
-
-        $(".modal-body a").click(function () {
-
-            if ($(this).hasClass("active")) {
-
-                //don't call server if same tab
-                return false;
-            }
-            else {
-                $(".modal-body a").removeClass("active");
-                $(this).addClass("active");
-
-                var getValue = $(this).text().trim().toLowerCase().toString();
-
-                getRaceInfo(getValue)
-            }         
+        //// select race tabs
+            MIM.selectRace();
 
         });
         
     });
 
  
-
-});
-// This optional function html-encodes messages for display in the page.
-function htmlEncode(value) {
-    var encodedValue = $('<div />').text(value).html();
-    return encodedValue;
-}
-
