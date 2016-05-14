0433488411a1ce168de63e55ab8d92b5bd854c3b
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-05-14 08:41:42 +0100

    work on movement

--- MIMEngine/Core/Events/Move.cs
+++ MIMEngine/Core/Events/Move.cs
@@ -14,11 +14,16 @@ namespace MIMEngine.Core.Events
     {
         public static void  MoveCharacter(PlayerSetup character, JObject room, string direction)
         {
-
             // char location
+            string regionName = character.Area;
+              string areaName = character.Area;
+              int roomId = character.AreaId;
 
             // check direction exists for the room the player in
 
+            JToken nextRoomInfo =  findExit(room, direction);
+
+            
             // change char location to new room
 
             // remove char from current room
@@ -26,7 +31,32 @@ namespace MIMEngine.Core.Events
             // add char to new room
 
             // send enter message to other players
-            
+
+        }
+
+        private static JToken findExit(JObject room, string direction)
+        {
+            var roomExitObj = room.Property("exits").Children();
+
+
+            string exitList = null;
+            foreach (var exit in roomExitObj)
+            {
+                if (exit["North"] != null)
+                {
+                    return exit["north"];
+                }
+
+                if (exit["East"] != null)
+                {
+                    return exit["east"];
+                }
+
+               
+
+            }
+
+            return "You cannot go that way";
         }
     }
 }
