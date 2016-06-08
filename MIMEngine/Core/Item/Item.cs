4e85ae94ce89d320342bc518af3f1ad4d2aa573b
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-06-08 23:33:24 +0100

    need an easier way to update the DB

--- MIMEngine/Core/Item/Item.cs
+++ MIMEngine/Core/Item/Item.cs
@@ -9,8 +9,10 @@ namespace MIMEngine.Core.Item
     public class Item :BaseItem
     {
         public List<Item> containerItems;
- 
- 
+        public string attackType { get; set; }
+        public string weaponType { get; set; }
+        public int weaponSpeed { get; set; }
+
     }
 
 
