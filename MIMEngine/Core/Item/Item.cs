b1905be7250dd1e0caef5087123dc8a0d75fda2c
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-06-09 07:56:44 +0100

    more item enums

--- MIMEngine/Core/Item/Item.cs
+++ MIMEngine/Core/Item/Item.cs
@@ -6,26 +6,82 @@ using System.Threading.Tasks;
 
 namespace MIMEngine.Core.Item
 {
-   
+    using System.ComponentModel;
+    using System.Runtime.Serialization;
+
     public class Item :BaseItem
     {
 
-        public enum AttackTypes
+        public enum AttackType
         {
             Slash,
+            Slice,
             Stab,
+            Thrust,
             Chop,
+            Claw,
+            Cleave,
+            Charge,
+            Crush,
+            Smash,
             Pierce,
-            Punch
+            Punch,
+            Pound,
+            Scratch,
+            Slap,
+            Whip,
+        }
+
+        public enum DamageType
+        {
+            Acidic,
+            Blast,
+            Chill,
+            Flame,
+            Flaming,
+            Freezing,
+            Shocking,
+            Holy,
+            Cursed,
+            Vampric,
+            Poisoned,
+            Stun
+        }
+
+        public enum WeaponType
+        {
+            Axe,
+            Dagger,
+            Exotic,
+            Flail,
+            Blunt,
+            Polearm,
+            Spear,
+            Staff,
+            Sword,
+            Whip,
+            Bows,
+            Arrows
+        }
+
+        public enum ArmourType
+        {
+            Cloth,
+            Leather,
+            [Description("Studded Leather")]
+            StuddedLeather,
+            [Description("Chain Mail")]
+            ChainMail,
+            [Description("Plate Mail")]
+            PlateMail,
+
+
         }
 
         public List<Item> containerItems;
-        public string attackType { get; set; }
+        public AttackType attackType { get; set; }
         public string weaponType { get; set; }
         public int weaponSpeed { get; set; }
 
     }
-
-
-
 }
