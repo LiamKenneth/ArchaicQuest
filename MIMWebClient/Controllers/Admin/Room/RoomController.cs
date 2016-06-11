13ed387ff1bd90b1f6cae50bb763e80057278850
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-06-11 14:54:13 +0100

    admin room controller

--- MIMWebClient/Controllers/Admin/Room/RoomController.cs
+++ MIMWebClient/Controllers/Admin/Room/RoomController.cs
@@ -6,12 +6,48 @@ using System.Web.Mvc;
 
 namespace MIMWebClient.Controllers.Admin.Room
 {
+    using MIMWebClient.Core.Room;
+
+    using MongoDB.Driver;
+
     public class RoomController : Controller
     {
-        // GET: Room
+        // GET: Admin
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            const string ConnectionString = "mongodb://localhost:27017";
+
+            // Create a MongoClient object by using the connection string
+            var client = new MongoClient(ConnectionString);
+
+            //Use the MongoClient to access the server
+            var database = client.GetDatabase("MIMDB");
+
+            var collection = database.GetCollection<Room>("Room");
+
+            var rooms = collection.Find(_ => true).ToList();
+
+            return this.View(rooms);
+        }
+
+        // GET: Admin/Details
+        [HttpGet]
+        public ActionResult Details(int areaid, string area)
+        {
+            const string ConnectionString = "mongodb://localhost:27017";
+
+            // Create a MongoClient object by using the connection string
+            var client = new MongoClient(ConnectionString);
+
+            //Use the MongoClient to access the server
+            var database = client.GetDatabase("MIMDB");
+
+            var collection = database.GetCollection<Room>("Room");
+
+            var rooms = collection.Find(x => x.area.Equals(area) && x.areaId.Equals(areaid)).FirstOrDefault();
+
+            return this.View(rooms);
         }
     }
 }
