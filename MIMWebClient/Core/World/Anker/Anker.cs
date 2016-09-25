054cae9305070a4231adaa161213a70c143cd39f
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-09-25 23:57:08 +0100

    work on room creation

---
+++ MIMWebClient/Core/World/Anker/Anker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIMWebClient.Core.World.Anker
+{
+    using MIMWebClient.Core.PlayerSetup;
+    using MIMWebClient.Core.Room;
+
+    public class Anker
+    {
+        public Room AnkerArea()
+        {
+            var room = new Room
+            {
+                region = "Anker",
+                area = "Anker",
+                areaId = 0,
+                title = "Market Sqaure",
+                description = "Description here",
+                mobs = new List<Player>()
+            };
+
+            var cat = new Player { Name = "Black and White cat" };
+
+            room.mobs.Add(cat);
+
+
+
+            return room;
+        }
+
+       
+    }
+}
