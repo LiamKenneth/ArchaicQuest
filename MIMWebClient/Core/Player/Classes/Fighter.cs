20fe4169a24013ee55ca739eaf5770ef56b9419c
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-10-08 17:35:15 +0100

    started on how to add playerclasses to game

---
+++ MIMWebClient/Core/Player/Classes/Fighter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIMWebClient.Core.Player.Classes
+{
+    using MIMWebClient.Core.Player.Skills;
+
+    public class Fighter : PlayerClass
+    {
+        public static PlayerClass FighterClass()
+        {
+
+
+            var fighter = new PlayerClass
+            {
+                Name = "Fighter",
+                ExperienceModifier = 0,
+                HelpText = "Help text about fighter class",
+                Skills = new List<Skill>(),
+                ReclassOptions = new List<PlayerClass>()
+            
+            };
+
+            var punch = Punch.PunchAb();
+
+            fighter.Skills.Add(punch);
+
+            return fighter;
+
+
+        }
+    }
+}
--- MIMWebClient/Core/Player/Classes/PlayerClass.cs
+++ MIMWebClient/Core/Player/Classes/PlayerClass.cs
@@ -7,5 +7,12 @@ namespace MIMWebClient.Core.Player.Classes
 {
     public class PlayerClass
     {
+        public string Name { get; set; }
+        public List<PlayerClass> ReclassOptions { get; set; } 
+        public List<Skill> Skills { get; set; } 
+        public int ExperienceModifier { get; set; }
+        public string HelpText { get; set; }
+
+        //add THAC0
     }
 }
