bc16c065cd58b5bfbc18128556a5df0665539b80
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-10-10 13:52:11 +0100

    Help command done

--- MIMWebClient/Core/Events/Help.cs
+++ MIMWebClient/Core/Events/Help.cs
@@ -15,24 +15,52 @@ namespace MIMWebClient.Core.Events
         public string DateUpdated;
         public List<Help> RelatedHelpFiles;
 
+        /// <summary>
+        /// List of available help files. Help is added to skills, spells and their related classes
+        /// Anything generic will be added to the help class
+        /// </summary>
+        /// <returns></returns>
         private static Dictionary<string, Help> HelpList()
         {
-            var helpList = new Dictionary<String, Help>
+            var helpList = new Dictionary<string, Help>
             {
-                {"Punch", Punch.PunchSkill.HelpText}
+                {"Help", GeneralHelp() },
+                {"Punch", Punch.PunchAb().HelpText}
             };
 
             return helpList;
         }
 
+        /// <summary>
+        /// Displays Help text to User
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <param name="player"></param>
         public static void ShowHelp(string keyword, PlayerSetup.Player player)
         {
             var help = HelpList().FirstOrDefault(x => x.Key.StartsWith(keyword, StringComparison.InvariantCultureIgnoreCase));
+
+            if (help.Value == null)
+            {
+                HubContext.SendToClient("No help found for '" + keyword + "'. The gods will be notified.", player.HubGuid);
+                //Logs missing help
+                return;
+            }
 
             var helpText = help.Value.Syntax + " " + help.Value.HelpText;
 
             HubContext.SendToClient(helpText, player.HubGuid);
         }
 
+        /// <summary>
+        /// General Help to MIM
+        /// </summary>
+        /// <returns></returns>
+        public static Help GeneralHelp()
+        {
+           var generalHelp = new Help {HelpText = "Welcome to MIM here are a list of commands to get you started...."};
+
+           return generalHelp;
+        }
     }
 }
