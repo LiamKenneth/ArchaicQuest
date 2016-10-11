75194b6583f0f66857796a456d0c0a38f82553d4
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2016-10-11 14:41:28 +0100

    Proof of concept on world timer, working as expected, Now for it to actually display and update the time
    Proof of concept on world timer, working as expected, Now for it to
    actually display and update the time

---
+++ MIMWebClient/Core/Update/UpdateWorld.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MIMWebClient.Core.Update
+{
+    public class UpdateWorld
+    {
+        public static void Init()
+        {
+            Task.Run(UpdateTime);
+        }
+
+        public static void CleanRoom()
+        {
+            Task.Run(UpdateRoom);
+        }
+
+        public static void UpdateMob()
+        {
+            Task.Run(MoveMob);
+        }
+        /// <summary>
+        /// Wahoo! This works
+        /// Now needs to update player/mob stats, spells, reset rooms and mobs. Move mobs around?
+        /// Global Timer every 60 seconds? quicker timer for mob movement?
+        /// </summary>
+        /// <returns></returns>
+        public static async Task UpdateTime()
+        {
+            await Task.Delay(60000);
+
+            HubContext.getHubContext.Clients.All.addNewMessageToPage("This is will update something every 60 seconds and not block the game");
+
+            Init();
+        }
+
+
+        public static async Task UpdateRoom()
+        {
+            await Task.Delay(5000);
+
+            HubContext.getHubContext.Clients.All.addNewMessageToPage("This is will update Rooms every 5 seconds and not block the game");
+
+            CleanRoom();
+        }
+
+        public static async Task MoveMob()
+        {
+            await Task.Delay(5000);
+
+            HubContext.getHubContext.Clients.All.addNewMessageToPage("This is will update Mobs every 5 seconds and not block the game");
+
+            UpdateMob();
+        }
+    }
+}
--- MIMWebClient/Global.asax.cs
+++ MIMWebClient/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MIMWebClient.Core.Update;
 
 namespace MIMWebClient
 {
@@ -16,6 +17,9 @@ namespace MIMWebClient
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UpdateWorld.Init();
+            UpdateWorld.CleanRoom();
+            UpdateWorld.UpdateMob();
         }
     }
 }
