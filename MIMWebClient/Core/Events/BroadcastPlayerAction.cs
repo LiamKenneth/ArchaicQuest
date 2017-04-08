b87f9a096eaa92d5c2b94a2bd0a5332cfbddfe7d
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2017-04-08 22:58:05 +0100

    deleted broadcast to room due to lack of control setting what individual players should see

--- MIMWebClient/Core/Events/BroadcastPlayerAction.cs
+++ MIMWebClient/Core/Events/BroadcastPlayerAction.cs
@@ -7,23 +7,7 @@ namespace MIMWebClient.Core.Events
 {
     public class BroadcastPlayerAction
     {
-        public static void BroadcastPlayerActions (string playerHub, string playerName, List<PlayerSetup.Player> playersInRoom, string messageForPlayer, string messageForRoom)
-        {
-            HubContext.SendToClient(messageForPlayer, playerHub);
-
-            var getPlayer = playersInRoom.FirstOrDefault(x => x.HubGuid.Equals(playerHub));
-           
-            foreach (var character in playersInRoom)
-            {
-                if (getPlayer != character)
-                {
-                  
-                    var roomMessage = $"{ Helpers.ReturnName(getPlayer, character, string.Empty)} says {messageForRoom}.";
-
-                    HubContext.SendToClient(roomMessage, character.HubGuid);
-                }
-            }
-        }
+      
        
     }
 }
